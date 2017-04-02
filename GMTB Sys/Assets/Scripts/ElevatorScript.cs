9c3d4d155fd9a34e9982e0f7bf361882db825b33
DittoDog12 <dittodog12@gmail.com>
2017-04-02 14:30:05 +0100

    Started Floors and Elevator
    Elevator should open a UI panel on collision but isn't currently

---
+++ GMTB Sys/Assets/Scripts/ElevatorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour
+{
+    public Vector2[] Floors;
+    public float speed;
+
+    public Vector2 TargetFloor;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ GMTB Sys/Assets/Scripts/ElevatorUIActivator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorUIActivator : MonoBehaviour
+{
+    public GameObject ElevatorUI;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ElevatorUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ElevatorUI.SetActive(false);
+        }
+    }
+}
