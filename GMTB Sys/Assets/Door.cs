43d8f2fce47b6de5ddd2d98a4732d0ac7780c378
DittoDog12 <dittodog12@gmail.com>
2017-04-14 11:58:38 +0100

    Started work on Door system
    Currently not recognising E press

---
+++ GMTB Sys/Assets/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 Destination;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Enter");
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E pressed");
+            Player.transform.position = Destination;
+        }
+    }
+}
