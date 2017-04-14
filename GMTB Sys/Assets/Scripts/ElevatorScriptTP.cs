4f1ce790967a1f8f0ff4ae44e48f38f4a30c697b
DittoDog12 <dittodog12@gmail.com>
2017-04-14 11:29:23 +0100

    New Elevators finished
    New "teleportation" style elevators

---
+++ GMTB Sys/Assets/Scripts/ElevatorScriptTP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScriptTP : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 newPosition;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MoveTo(float floor)
+    {
+        newPosition.y = floor;
+        newPosition.z = player.transform.position.z;
+        player.transform.position = newPosition;
+    }
+    public void setShaft(float shaft)
+    {
+        newPosition.y = shaft;
+    }
+}
