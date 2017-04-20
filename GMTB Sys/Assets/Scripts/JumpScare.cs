24c461778ceb4e939bb96838290254f61d8dfb23
DittoDog12 <dittodog12@gmail.com>
2017-04-20 15:49:52 +0100

    Jumpscare + Stairs implemented, Elevators altered

---
+++ GMTB Sys/Assets/Scripts/JumpScare.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScare : MonoBehaviour
+{
+    public GameObject HidingBG;
+    public GameObject JumpBG;
+    public GameObject Trigger;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            HidingBG.SetActive(false);
+            JumpBG.SetActive(true);
+            DestroyObject(HidingBG);
+            DestroyObject(Trigger);
+            Destroy(this);
+        }
+            
+    }
+}
+
---
+++ GMTB Sys/Assets/Scripts/PowerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerController : MonoBehaviour
+{
+    public GameObject ElevatorDeck1;
+    public GameObject ElevatorDeck2;
+    public GameObject ElevatorDeck3R;
+    public GameObject ElevatorDeck3L;
+    public GameObject ElevatorDeck4L;
+    public GameObject ElevatorDeck4R;
+    public GameObject ElevatorDeck5L;
+    public GameObject ElevatorDeck5R;
+    public GameObject ElevatorDeck6;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PowerOn()
+    {
+        ElevatorDeck1.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck2.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck3R.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck3L.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck4L.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck4R.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck5L.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck5R.GetComponent<Collider2D>().enabled = true;
+        ElevatorDeck6.GetComponent<Collider2D>().enabled = true;
+    }
+
+}
