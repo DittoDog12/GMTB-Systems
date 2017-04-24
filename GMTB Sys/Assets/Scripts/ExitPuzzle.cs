5cfd4487535f9c3fecb25879b20ac19c3e31f7ce
DittoDog12 <dittodog12@gmail.com>
2017-04-24 14:21:48 +0100

    Puzzle transition working both directions

---
+++ GMTB Sys/Assets/Scripts/ExitPuzzle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitPuzzle : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Puzzle;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Click()
+    {
+        Puzzle.SetActive(false);
+        Player.SetActive(true);
+    }
+}
